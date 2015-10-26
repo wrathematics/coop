e20853e00e3e7d5f5e6e47231a5b1e41db8f6672
Drew Schmidt <schmidt@math.utk.edu>
2015-10-26 15:48:21 -0400

    add a square test

--- tests/test.R
+++ tests/test.R
@@ -12,3 +12,10 @@ t1 <- lsa::cosine(x)
 t2 <- fastcosim::cosine(x)
 stopifnot(all.equal(t1, t2, check.attributes=FALSE))
 
+
+
+x <- tcrossprod(x)
+
+t1 <- lsa::cosine(x)
+t2 <- fastcosim::cosine(x)
+stopifnot(all.equal(t1, t2, check.attributes=FALSE))
