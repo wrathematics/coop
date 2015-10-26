90dc6687dfb03d30abd278507a87300eadf1c6e0
wrathematics <wrathematics@gmail.com>
2015-10-26 10:24:35 -0400

    added tests

---
+++ tests/test.R
@@ -0,0 +1,14 @@
+x <- matrix(rnorm(30), 10)
+
+t1 <- lsa::cosine(x)
+t2 <- fastcosim::cosine(x)
+stopifnot(all.equal(t1, t2, check.attributes=FALSE))
+
+
+
+x <- t(x)
+
+t1 <- lsa::cosine(x)
+t2 <- fastcosim::cosine(x)
+stopifnot(all.equal(t1, t2, check.attributes=FALSE))
+
