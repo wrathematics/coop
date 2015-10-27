908124c05fb514dc217cb036c9845e5d38f3460d
wrathematics <wrathematics@gmail.com>
2015-10-27 11:14:36 -0400

    better documentation

--- R/cosine.r
+++ R/cosine.r
@@ -13,10 +13,26 @@ NULL
 
 #' Cosine Similarity
 #' 
-#' Compute cosine similarity efficiently.
+#' Compute the cosine similarity matrix efficiently.
+#' 
+#' @details
+#' The function computes the cosine similarity between all column
+#' vectors of the numeric input matrix
 #' 
 #' @param x
 #' A numeric matrix.
+#' 
+#' @return
+#' The \eqn{n\times n} matrix of all pair-wise vector cosine
+#' similarities of the columns.  The implementation is dominated
+#' by a symmetric rank-k update via the BLAS function dsyrk(); see
+#' the package \code{README.md} file for a discussion of the 
+#' algorithm implementation and complexity.
+#' 
+#' @examples
+#' library(fastcosim)
+#' x <- matrix(rnorm(10*3), 10, 3)
+#' cosine(x)
 #' 
 #' @export
 cosine <- function(x)
