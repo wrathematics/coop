20b6e12a108cd6c2487809995e0b3f96ae9514f7
Drew Schmidt <schmidt@math.utk.edu>
2016-03-05 19:15:53 -0500

    better benchmarks

--- inst/benchmarks/pcor.r
+++ inst/benchmarks/pcor.r
@@ -1,8 +1,10 @@
 library(fastco)
 library(rbenchmark)
+cols <- cols <- c("test", "replications", "elapsed", "relative")
+reps <- 25
 
-m <- 2000
-n <- 200
+m <- 10000
+n <- 250
 x <- matrix(rnorm(m*n), m, n)
 
-benchmark(cor(x), pcor(x), replications=100)
+benchmark(cor(x), pcor(x), replications=reps, columns=cols)
