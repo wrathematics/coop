350544bb732846bfed2e701803761a2b9d8b53c1
wrathematics <wrathematics@gmail.com>
2016-05-05 00:02:05 -0400

    fix scaler test, finish help

--- R/scaler.r
+++ R/scaler.r
@@ -9,6 +9,14 @@
 #' \code{center} and \code{scale} can only be logical values
 #' (and not vectors).
 #' 
+#' @param x
+#' The input matrix.
+#' @param center,scale
+#' Logical; determine if the data should be centered and/or scaled.
+#' 
+#' @return
+#' The centered/scaled data, with attributes as in R's \code{scale()}.
+#' 
 #' @export
 scaler <- function(x, center=TRUE, scale=TRUE)
 {
@@ -22,5 +30,8 @@ scaler <- function(x, center=TRUE, scale=TRUE)
   if (!is.double(x))
     storage.mode(x) <- "double"
   
-  .Call(R_scaler, center, scale, x)
+  if (!center && !scale)
+    return(x)
+  else
+    .Call(R_scaler, center, scale, x)
 }
