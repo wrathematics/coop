b57d54d6ed000ccd852748bcec54e401c8ae8e42
Drew Schmidt <wrathematics@gmail.com>
2016-07-17 18:00:09 -0400

    modify covariance R benchmark

--- inst/benchmarks/covar_dense.r
+++ inst/benchmarks/covar_dense.r
@@ -7,4 +7,9 @@ m <- 10000
 n <- 250
 x <- matrix(rnorm(m*n), m, n)
 
+cat("### regular\n")
 benchmark(cov(x), covar(x), replications=reps, columns=cols)
+
+cat("### pairwise\n")
+benchmark(cov(x, use='pair'), covar(x, use='pair'), replications=reps, columns=cols)
+
