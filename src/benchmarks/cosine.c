af2c6e1fa51a1669db7d30852ad0b9f51cc83a19
wrathematics <wrathematics@gmail.com>
2016-09-21 23:09:11 -0400

    add cosine benchmark to src tree

---
+++ src/benchmarks/cosine.c
@@ -0,0 +1,21 @@
+#include "../utils/internal/gen.h"
+#include "../utils/internal/timer.h"
+
+#include <stdbool.h>
+
+int main()
+{
+  const int nreps = 100;
+  const int m = 10000;
+  const int n = 250;
+  double *x, *co;
+  
+  x = gen_runif2(m*n);
+  co = zeromat2(n*n);
+  
+  TIMER(coop_cosine_mat(false, false, m, n, x, co), nreps);
+  
+  free(x);
+  free(co);
+  return 0;
+}
